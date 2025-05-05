a202ccbed183f0ef0e0a1dc09d38ae86eea4691d
= <jonhur@web.de>
2025-05-05 12:05:34 +0200

    added additional checks

--- openeo_processes_dask_ml/process_implementations/load_model.py
+++ openeo_processes_dask_ml/process_implementations/load_model.py
@@ -1,18 +1,32 @@
 import re
 import requests
 import requests.exceptions
+import pystac
 from stac_validator.validate import StacValidate
 
+AVAILABLE_ML_FRAMEWORKS: list[str] = []
 
-def load_ml_model(uri: str, model_asset: str = None):
+from .data_model import MLModel
+try:
+    from .data_model import ONNXModel
+    AVAILABLE_ML_FRAMEWORKS.append("ONNX")
+except ModuleNotFoundError:
+    pass
+
+
+def load_ml_model(uri: str, model_asset: str = None) -> MLModel:
     if type(uri) is not str:
         raise ValueError("Type of URI parameter must be a string.")
+
+    # todo: uri could be a local URI
 
     # fetch STAC Item
     r = requests.get(uri)
     if r.status_code != 200:
-        raise requests.exceptions.HTTPError("Error while fetching STAC Item from URI: "
-                                            "Server did not respond with status code 200")
+        raise requests.exceptions.HTTPError(
+            "Error while fetching STAC Item from URI: "
+            "Server did not respond with status code 200"
+        )
 
     try:
         stac = r.json()
@@ -37,11 +51,33 @@ def load_ml_model(uri: str, model_asset: str = None):
     follows_mlm = any(pattern.match(s) for s in extensions)
     if not follows_mlm:
         raise Exception("The provided STAC Item does not implement the STAC:MLM standard")
-    # todo: validate json against extension schema
 
-    # parse STAC:MLM item
+    mlm_item = pystac.Item.from_dict(stac)
+
+    # check if the item's extensions are valid (e
+    try:
+        mlm_item.validate()
+    except pystac.errors.STACValidationError:
+        raise Exception(
+            "The provided STAC Item does not implement STAC:MLM extension correctly."
+        )
 
     # Check if model runtime is supported (ONNX!, torch? tf?)
+    ml_framework = mlm_item.ext.mlm.framework
+
+    if ml_framework not in AVAILABLE_ML_FRAMEWORKS:
+        raise Exception(
+            f"The ML framework {ml_framework} as required by the provided STAC:MLM Item"
+            f"is not supported by this backend. Supported backends: "
+            f"{', '.join(AVAILABLE_ML_FRAMEWORKS)}"
+        )
+
+    if ml_framework == "ONNX":
+        model_object = ONNXModel(mlm_item)
+    else:
+        raise Exception(
+            f"{ml_framework} runtime is not supported."
+        )
 
     # download model
     # question: Download here, or when mdoel is actually executed?
@@ -50,4 +86,4 @@ def load_ml_model(uri: str, model_asset: str = None):
 
     # construct model from object
 
-    return uri
+    return model_object
