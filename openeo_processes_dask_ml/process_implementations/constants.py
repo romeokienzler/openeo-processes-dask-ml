c81a8b5853c55f88bc23b96bdbd878abf04eb997
= <jonhur@web.de>
2025-05-14 16:05:01 +0200

    updated constants for ENVs

--- openeo_processes_dask_ml/process_implementations/constants.py
+++ openeo_processes_dask_ml/process_implementations/constants.py
@@ -1,3 +1,14 @@
 import os
 
 MODEL_CACHE_DIR = os.environ.get("MODEL_CACHE_DIR", "./cache/model_cache")
+
+S3_MODEL_REPO_ENDPOINT = os.environ.get("S3_MODEL_REPO_ENDPOINT", None)
+S3_MODEL_REPO_ACCESS_KEY_ID = os.environ.get("S3_MODEL_REPO_ACCESS_KEY_ID", None)
+S3_MODEL_REPO_SECRET_ACCESS_KEY = os.environ.get("S3_MODEL_REPO_SECRET_ACCESS_KEY", None)
+
+# make sure that both access_key_id and access_key are set
+if (S3_MODEL_REPO_ACCESS_KEY_ID is None) != (S3_MODEL_REPO_SECRET_ACCESS_KEY is None):
+    raise ValueError(
+        "Either S3_MODEL_REPO_ACCESS_KEY_ID or S3_MODEL_REPO_SECRET_ACCESS_KEY is set "
+        "not set. You must set either both, or not set any of the two."
+    )
