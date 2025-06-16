c3fe79dc26611fe0660610cb4d3cb37cb4640792
= <jonhur@web.de>
2025-06-16 11:44:32 +0200

    added dummy execute method

--- tests/dummy/dummy_ml_model.py
+++ tests/dummy/dummy_ml_model.py
@@ -1,4 +1,6 @@
 from openeo_processes_dask_ml.process_implementations.data_model import MLModel
+import xarray as xr
+import numpy as np
 
 
 class DummyMLModel(MLModel):
@@ -7,5 +9,25 @@ class DummyMLModel(MLModel):
     def create_model_object(self, filepath: str):
         pass
 
-    def execute_model(self, batch):
-        pass
+    def execute_model(self, batch: xr.DataArray) -> xr.DataArray:
+        out_shape = self.model_metadata.output[0].result.shape
+        out_dims = self.model_metadata.output[0].result.dim_order
+        out_dtype = self.model_metadata.output[0].result.data_type
+
+        out_dc_shape = [*out_shape]
+
+        # replace batch dim -1 with actual batch number from input
+        if "batch" in out_dims and out_shape[out_dims.index("batch")] == -1:
+            in_batch_idx = self.model_metadata.input[0].input.dim_order.index("batch")
+            n_batches = batch.shape[in_batch_idx]
+
+            out_batch_idx = out_dims.index("batch")
+            out_dc_shape[out_batch_idx] = n_batches
+
+        print(out_dc_shape)
+
+        r = xr.DataArray(
+            np.random.random(out_dc_shape).astype(out_dtype),
+            dims=out_dims
+        )
+        return r
