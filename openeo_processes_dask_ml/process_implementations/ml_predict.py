52a1d2b1db3ed9031575f6737a3fd263641a6379
= <jonhur@web.de>
2025-06-30 11:44:26 +0200

    added prediction code

--- openeo_processes_dask_ml/process_implementations/ml_predict.py
+++ openeo_processes_dask_ml/process_implementations/ml_predict.py
@@ -2,5 +2,8 @@ import xarray as xr
 from .data_model import MLModel
 
 
-def ml_predict(data: xr.DataArray, model: MLModel, dimension: list[str]):
-    pass
+def ml_predict(
+        data: xr.DataArray, model: MLModel, dimension: list[str]
+) -> xr.DataArray:
+    out = model.run_model(data)
+    return out
