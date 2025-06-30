7c87efc64b39c0066cb7edd105889da7adc8fbbb
= <jonhur@web.de>
2025-06-30 11:45:51 +0200

    add todo

--- openeo_processes_dask_ml/process_implementations/data_model/torch_model.py
+++ openeo_processes_dask_ml/process_implementations/data_model/torch_model.py
@@ -5,10 +5,11 @@ import torch
 
 class TorchModel(MLModel):
     def create_model_object(self, filepath: str):
+        # todo: consider checkpoint, JIT, export
         self._model_object = torch.jit.load(filepath)
         self._model_object.eval()
 
     def execute_model(self, batch):
         tensor = torch.from_numpy(batch.data)
         with torch.no_grad():
-            self._model_object(tensor)
+            out = self._model_object(tensor)
