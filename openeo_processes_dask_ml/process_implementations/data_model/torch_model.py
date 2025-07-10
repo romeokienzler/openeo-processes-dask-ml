ea94b98d2144e5dd6ae17372ad8ee9fc9e24bf57
= <jonhur@web.de>
2025-07-10 16:37:27 +0200

    implemented execute_model function

--- openeo_processes_dask_ml/process_implementations/data_model/torch_model.py
+++ openeo_processes_dask_ml/process_implementations/data_model/torch_model.py
@@ -1,6 +1,7 @@
 from .data_model import MLModel
 
 import torch
+import xarray as xr
 
 
 class TorchModel(MLModel):
@@ -9,7 +10,20 @@ class TorchModel(MLModel):
         self._model_object = torch.jit.load(filepath)
         self._model_object.eval()
 
-    def execute_model(self, batch):
+    def execute_model(self, batch: xr.DataArray) -> xr.DataArray:
         tensor = torch.from_numpy(batch.data)
         with torch.no_grad():
             out = self._model_object(tensor)
+
+        self.postprocess_datacube_expression(out)
+
+        out_postproc = self.postprocess_datacube_expression(out)
+        # todo: what if input is not batched?
+        # add an artificial batch dimension
+
+        out_dims = self.model_metadata.output[0].result.dim_order
+        out_cube = xr.DataArray(
+            out_postproc.numpy(),
+            dims=out_dims
+        )
+        return out_cube
