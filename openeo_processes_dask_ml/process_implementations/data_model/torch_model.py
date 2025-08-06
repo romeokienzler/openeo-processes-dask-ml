61a8341bf1cad6f0e09306ec6943326f140ebc19
= <jonhur@web.de>
2025-08-06 16:16:35 +0200

    free up gpu device memory after prediction is finished

--- openeo_processes_dask_ml/process_implementations/data_model/torch_model.py
+++ openeo_processes_dask_ml/process_implementations/data_model/torch_model.py
@@ -1,29 +1,41 @@
 import torch
 import xarray as xr
 
+import pystac
+
 from .data_model import MLModel
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-# DEVICE = "cpu"
 
 
 class TorchModel(MLModel):
+    def __init__(self, stac_item: pystac.Item, model_asset_name: str = None):
+        MLModel.__init__(self, stac_item, model_asset_name)
+
+        self._model_on_device = None
+
     def create_model_object(self, filepath: str):
         # todo: consider checkpoint, JIT, export
         self._model_object = torch.jit.load(filepath)
-        self._model_object = self._model_object.to(DEVICE)
-        self._model_object.eval()
+
+    def init_model_for_prediction(self):
+        self._model_on_device = self._model_object.to(DEVICE)
+        self._model_on_device.eval()
+
+    def uninit_model_after_prediction(self):
+        self._model_on_device = self._model_on_device.to("cpu")
+        del self._model_on_device
+        self._model_on_device = None
+        torch.cuda.empty_cache()
 
     def execute_model(self, batch: xr.DataArray) -> xr.DataArray:
         tensor = torch.from_numpy(batch.data).to(DEVICE)
         with torch.no_grad():
-            out = self._model_object(tensor)
+            out = self._model_on_device(tensor)
 
         self.postprocess_datacube_expression(out)
 
         out_postproc = self.postprocess_datacube_expression(out)
-        # todo: what if input is not batched?
-        # add an artificial batch dimension
 
         out_dims = self.model_metadata.output[0].result.dim_order
         out_cube = xr.DataArray(out_postproc.numpy(), dims=out_dims)
