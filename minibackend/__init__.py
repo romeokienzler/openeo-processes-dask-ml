20829adb288aaa6f5dc2c9fa749701e0a3f07916
= <jonhur@web.de>
2025-09-12 10:55:08 +0200

    renamed function

--- minibackend/__init__.py
+++ minibackend/__init__.py
@@ -1,1 +1,1 @@
-from .execute_graph import execute_graph
+from .execute_graph import execute_graph_dict, execute_graph_file
--- opd_ml_dev_utils/execute.py
+++ opd_ml_dev_utils/execute.py
@@ -4,11 +4,11 @@ Execute a process graph with ML processes using the provided minibackend
 
 import os
 
-from minibackend import execute_graph
+from minibackend import execute_graph_file
 
 if os.getcwd().split("/")[-1] == "opd_ml_dev_utils":
     os.chdir("..")
 
 GRAPH_PATH = "opd_ml_dev_utils/graph.json"
-result = execute_graph(GRAPH_PATH)
+result = execute_graph_file(GRAPH_PATH)
 print(result)
